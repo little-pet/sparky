c8a916420caf8af5f02ef5c745e50f9c8f9bbb49
dbugakov <you@example.com>
2019-02-22 16:03:58 +0300

    add some docs

--- src/main/scala/com/sparky/schema/package.scala
+++ src/main/scala/com/sparky/schema/package.scala
@@ -2,7 +2,15 @@ package com.sparky
 
 import org.apache.spark.sql.types.{StructField, StructType}
 
+/** Provides functions for dealing with schemas. */
 package object schema {
+  /** Compares two schemas
+    *
+    * @param s1                schema 1
+    * @param s2                schema 2
+    * @param ignoreNullable    flag include nullable check
+    * @param ignoreColumnNames flag include names check
+    */
   def customEquals(s1: StructType, s2: StructType)
                   (ignoreNullable: Boolean)
                   (ignoreColumnNames: Boolean): Boolean = {
